3406b2f7f8a10dcc24620403e6402f5e4df180b3
touhid-imam <touhid.imam1337@gmail.com>
2023-12-11 02:23:27 -0600

    Day 1: Band name generator project

---
+++ day1-band-name-generator/main.py
@@ -0,0 +1,5 @@
+print("\n #Welcome to the band name generator# \n")
+your_city = input("What's name of the city you grew up in? \n")
+your_pet_name = input("What's your pet's name? \n")
+
+print("Your band name could be " + your_city + " " + your_pet_name)
